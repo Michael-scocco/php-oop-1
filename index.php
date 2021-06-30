a41b21ead78df6dafc82f61696ffe396778e6269
Michael Scocco <scocco.michael87@gmail.com>
2021-06-30 09:56:34 +0200

    finito

--- index.php
+++ index.php
@@ -1,78 +1,129 @@
   
-<?php  
-    class Movie {
-        public $name;
-        public $language;
-        public $year;
-        public $genre;
+<?php 
 
-        function __construct(string $name, string $language, int $year, string $genre) {
-            $this->name = $name;
-            $this->language = $language;
-            $this->year = $year;
-            $this->genre = $genre;
-        }
 
-        public function yearsSinceRelease() {
-            return date("Y") - $this->year;
-        }
-    }
+class Film {
+
+  public $title;
+  public $director;
+  public $year;
+  public $budget;
+  public $language;
+
+  function __construct(string $title, string $director, int $year, int $budget, string $language){
+    $this->title = $title;
+    $this->director = $director;
+    $this->year = $year;
+    $this->budget = $budget;
+    $this->language = $language;
+  }
+ 
+
+  /**
+   * Get the value of title
+   */ 
+  public function getTitle()
+  {
+    return $this->title;
+  }
+
+  /**
+   * Set the value of title
+   *
+   * @return  self
+   */ 
+  public function setTitle($title)
+  {
+    $this->title = $title;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of director
+   */ 
+  public function getDirector()
+  {
+    return $this->director;
+  }
+
+  /**
+   * Set the value of director
+   *
+   * @return  self
+   */ 
+  public function setDirector($director)
+  {
+    $this->director = $director;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of year
+   */ 
+  public function getYear()
+  {
+    return $this->year;
+  }
+
+  /**
+   * Set the value of year
+   *
+   * @return  self
+   */ 
+  public function setYear($year)
+  {
+    $this->year = $year;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of budget
+   */ 
+  public function getBudget()
+  {
+    return $this->budget;
+  }
+
+  /**
+   * Set the value of budget
+   *
+   * @return  self
+   */ 
+  public function setBudget($budget)
+  {
+    $this->budget = $budget;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of language
+   */ 
+  public function getLanguage()
+  {
+    return $this->language;
+  }
+
+  /**
+   * Set the value of language
+   *
+   * @return  self
+   */ 
+  public function setLanguage($language)
+  {
+    $this->language = $language;
+
+    return $this;
+  }
+};
+
+  $avengersEndgame = new Film("Avengers: Endgame", "Joe Russo", 2019, 300, "eng");
+  $smettoQuandoVoglio = new Film("Smetto Quando Voglio", "Sydney Sibilia", 2014, 200, "ita");
+  $batman = new Film("Batman", "Tim Burton", 1989, 100, "eng");
+
+  var_dump($avengersEndgame, $smettoQuandoVoglio, $batman);
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Object Oriented Programming</title>
-    <style>
-        .container {
-            display: flex;
-            gap: 30px;
-            justify-content: center;
-        }
-
-        .movie {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            background-color: lavender;
-            padding: 20px;
-        }
-    </style>
-</head>
-<body>
-    <?php
-        $starWars = new Movie("Star Wars", "English", 1977, "Science fiction");
-        $lordOfTheRings = new Movie("The Lord of the Rings: The Fellowship of the Ring", "English", 2001, "Fantasy");
-        $americanPsycho = new Movie("American Psycho", "English", 2000, "Horror");
-        $matrix = new Movie("The Matrix", "English", 1999, "Science fiction");
-
-        $movies = [
-            $starWars,
-            $lordOfTheRings,
-            $americanPsycho,
-            $matrix
-        ];
-    ?>
-
-    <div class="container">
-        <?php 
-            foreach($movies as $movie) {
-                echo "
-                <div class='movie'>
-                    <div>{$movie->name}</div>
-                    <div>{$movie->language}</div>
-                    <div>{$movie->genre}</div>
-                    <div class='release'>
-                        <span>{$movie->year}</span>
-                        <span>({$movie->yearsSinceRelease()} years ago)</span>
-                    </div>
-                </div>
-                ";
-            }
-        ?>
-    </div>
-</body>
-</html>
